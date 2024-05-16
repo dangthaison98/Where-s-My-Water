a5b910d000b7ee16d14662bdd103629ffab1f393
Thaisond98 <147731298+dangthaison98@users.noreply.github.com>
2024-05-16 16:16:51 +0700

    Add script

---
+++ Assets/Scripts/AnimalRotate.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalRotate : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log("Down");
+    }
+    private void OnMouseDrag()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ - 90);
+    }
+    private void OnMouseUp()
+    {
+        Debug.Log("Up");
+    }
+}
