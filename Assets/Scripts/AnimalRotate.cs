9c9364e1ef1f19b5af61df61a428948e2b5e63ff
Thaisond98 <147731298+dangthaison98@users.noreply.github.com>
2024-05-17 09:24:48 +0700

    Add tool

--- Assets/Scripts/AnimalRotate.cs
+++ Assets/Scripts/AnimalRotate.cs
@@ -1,10 +1,10 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimalRotate : MonoBehaviour
 {
-    public int animalLength;
 
     private void OnMouseDown()
     {
@@ -19,5 +19,31 @@ public class AnimalRotate : MonoBehaviour
     private void OnMouseUp()
     {
         Debug.Log("Up");
+    }
+
+
+
+    [Range(0, 10)]
+    public int animalLength;
+    public CapsuleCollider2D capsuleCollider;
+    public GameObject head;
+    public GameObject body;
+    [Button(ButtonHeight = 100)]
+    public void BakeAnimal()
+    {
+        head.transform.localPosition = new Vector3(0, 0.3f + 0.8659766f * animalLength, 0);
+        if(animalLength == 0)
+        {
+            body.SetActive(false);
+        }
+        else
+        {
+            body.SetActive(true);
+            body.transform.position = new Vector3(0, (0.8659766f * animalLength)/2 + 0.3f, 0);
+            body.transform.localScale = new Vector3(0.8659766f * 4 * animalLength, 1, 1);
+        }
+
+        capsuleCollider.offset = new Vector2 (0, 0.8659766f * (animalLength + 1)/2);
+        capsuleCollider.size = new Vector2 (0.5f, 0.8659766f * (animalLength + 1) + 0.5f);
     }
 }
