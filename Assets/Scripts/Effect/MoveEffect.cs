1a1aef0c3b743c8363c36407a7dd3bdfa31690c0
Thaisond98 <147731298+dangthaison98@users.noreply.github.com>
2024-05-24 18:40:57 +0700

    Add tutorial

--- Assets/Scripts/Effect/MoveEffect.cs
+++ Assets/Scripts/Effect/MoveEffect.cs
@@ -7,12 +7,15 @@ public class MoveEffect : MonoBehaviour
 {
     Tween currentTween;
 
+    public CycleMode cycleMode;
+    public float duration;
+
     public Transform pos1;
     public Transform pos2;
 
     private void OnEnable()
     {
-        currentTween = Tween.Position(transform, startValue: pos1.position, endValue: pos2.position, duration: 0.5f, cycles: -1, cycleMode: CycleMode.Yoyo, useUnscaledTime: true);
+        currentTween = Tween.Position(transform, startValue: pos1.position, endValue: pos2.position, duration: duration, cycles: -1, cycleMode: cycleMode, useUnscaledTime: true);
     }
     private void OnDisable()
     {
